abdf2c48409dd4db65636a8e20d03bfdb1e689b6
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-12 09:29:55 -0300

    Criei import externo

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import CadastroItens from "./cadastroItens";
+import CadastroProduto from "./cadastroProduto";
+import HistoricoCadastro from "./historicoCadastro";
+import ListaProduto from "./listaProduto";
+
+export {CadastroItens, CadastroProduto, HistoricoCadastro, ListaProduto};
