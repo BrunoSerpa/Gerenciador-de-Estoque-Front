73c42d67831c1ce8aa01fb80e1258a5f575fce35
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-12 09:23:54 -0300

    Criei o hook de navegação

---
+++ src/hooks/useNavegation.ts
@@ -0,0 +1,15 @@
+import ParamPages from "../context/ParamPages";
+import { navigationRef } from "../refs/navigationRef";
+
+export const navigateTo = <RouteName extends keyof ParamPages>(
+    routeName: RouteName,
+    params?: ParamPages[RouteName]
+) => {
+    if (navigationRef.current) {
+        if (params) {
+            navigationRef.current.navigate(routeName as any, params);
+        } else {
+            navigationRef.current.navigate(routeName as any);
+        }
+    }
+};
