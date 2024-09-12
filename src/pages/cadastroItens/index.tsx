95c8c7ebb17080e7454ff6ea2bdb66a02d503601
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-12 09:26:57 -0300

    Inseri parâmetro do id do produto

--- src/pages/cadastroItens/index.tsx
+++ src/pages/cadastroItens/index.tsx
@@ -1,9 +1,16 @@
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { Text, View } from "react-native";
+import ParamPages from "../../context/ParamPages";
+
+type ItensRouteProp = RouteProp<ParamPages, 'Cadastro de Itens'>;
 
 export default function CadastroItens() {
+    const route = useRoute<ItensRouteProp>()
+    const { produtoId } = route.params || 0;
     return (
         <View>
             <Text>Cadastro de Itens</Text>
+            {produtoId && <Text>ID do produto {produtoId} </Text>}
         </View>
     );
 };
