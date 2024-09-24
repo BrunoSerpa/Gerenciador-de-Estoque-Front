cd93b511d1c87ccee8e03180f046879fdef02c26
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-24 19:30:34 -0300

    Inseri mais cores no tema

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,13 @@ const theme = {
     cinza1: "#DFDFDF",
     cinza2: "#868686",
     cinza3: "#696969",
-    preto1: "#2F2F2F"
+    preto1: "#2F2F2F",
+    verde1: '#27C561',
+    verde2: '#2EB151',
+    vermelho1: '#DD4545',
+    amarelo1: '#FCD443',
+    azul1: '#1D75AC',
+    transparente: "rgba(42, 42, 42, 0.6)"
 };
 
 export default theme;
