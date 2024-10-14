7dd5607964e8a01fe153adc0aa26085befd007b4
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-14 10:08:53 -0300

    Estilizei o formsLote

--- src/components/forms/style.ts
+++ src/components/forms/style.ts
@@ -14,6 +14,15 @@ export const styleForms = StyleSheet.create({
         paddingHorizontal: 30,
         paddingVertical: 3,
     },
+    viewAdicionar: {
+        width: "90%",
+        marginTop: 2
+    },
+    botaoAdicionar: {
+        paddingHorizontal: 30,
+        paddingVertical: 3,
+        ...textoStyle.texto3
+    },
     viewBotao: {
         alignItems: 'center',
         display: 'flex',
@@ -25,14 +34,74 @@ export const styleForms = StyleSheet.create({
         justifyContent: 'space-between',
         paddingHorizontal: 10
     },
+    viewFlex: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    viewForms: {
+        marginTop: 20,
+        marginLeft: 10
+    },
+    viewExcluir: {
+        width: 90
+    },
+    excluirIcon: {
+        marginRight: 4,
+        width: 30,
+        height: 30,
+    },
     textErro: {
         color: theme.vermelho1,
-        textAlign:  'center',
+        textAlign: 'center',
         ...textoStyle.texto1
     },
     textSucesso: {
         color: theme.preto1,
-        textAlign:  'center',
+        textAlign: 'center',
         ...textoStyle.texto1
     }
 });
+
+const constList = StyleSheet.create({
+    widthProduto: {
+        flex: 1,
+    },
+    widhtPreco: {
+        width: '28%',
+    },
+    widthQuantidade: {
+        width: '28%',
+    },
+    separator: {
+        backgroundColor: theme.preto1,
+        height: "100%",
+        width: 2.5,
+    },
+})
+
+export const styleItens = StyleSheet.create({
+    viewLinha: {
+        borderTopWidth: 0,
+        borderWidth: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        width: '90%',
+    },
+    ...constList
+})
+
+export const styleTitulo = StyleSheet.create({
+    viewTitulo: {
+        borderWidth: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        width: '90%',
+    },
+    texts: {
+        margin:'auto',
+        textAlign: 'center',
+        fontWeight: '500',
+        ...textoStyle.texto3
+    },
+    ...constList
+})
