5b5c33c611f3af1db0088d4c4303627f95eafb0e
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-14 10:36:36 -0300

    Criei a página atualizarCadastro

---
+++ src/pages/atualizarCadastro/index.tsx
@@ -0,0 +1,16 @@
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { Text, View } from "react-native";
+import ParamPages from "../../interface/ParamPages";
+import { principalView } from "../../styles";
+
+type ItensRouteProp = RouteProp<ParamPages, 'Atualizar Cadastro'>;
+
+export default function AtualizarCadastro() {
+    const route = useRoute<ItensRouteProp>()
+    const { cadastroId } = route.params || 0;
+    return (
+        <View style={principalView.PrincipalView}>
+            <Text style={principalView.titleRoute}>Atualizar Cadastro (ID: {cadastroId})</Text>
+        </View>
+    );
+};
