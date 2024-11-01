13c61b9a64417530fa2a55df14ac4d8b3e28edbc
BrunoSerpa <serpabruno2005@gmail.com>
2024-11-01 11:00:49 -0300

    Criei os checkbox da lista de itens

--- src/components/lists/listaItens/index.tsx
+++ src/components/lists/listaItens/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import { listarItens } from "../../../services";
 import { VisualizarItem } from "../../../interface/Item";
 import { styleLista } from "../style";
 import Carregando from "../../carregando";
 import Titulo from "./titulo";
 import Item from "./item";
+import ICheckbox from "./interface";
+import { Checkbox } from "../../inputs";
 
 interface Props {
     id_produto: number
@@ -25,26 +27,49 @@ export default function ListaItens(item: Props) {
         setRefresh(false);
     }, [refresh]);
 
+    const [showData, setShowData] = useState(true)
+    const [showExcluir, setShowExcluir] = useState(true)
+    const [showID, setShowID] = useState(true)
+    const [showPreco, setShowPreco] = useState(true)
+    const checkbox: ICheckbox = {
+        Data: showData,
+        Excluir: showExcluir,
+        ID: showID,
+        Preco: showPreco
+    }
     return (
         produtos === undefined ?
             <Carregando />
             :
-            <ScrollView
-                style={styleLista.scrollView}
-                horizontal={true}
-            >
+            <>
+                <View style={styleLista.viewCheckboxPrincipal}>
+                    <View style={styleLista.viewCheckbox}>
+                        <Checkbox set={setShowID} status={showID} titulo="Mostrar ID" />
+                        <Checkbox set={setShowData} status={showData} titulo="Mostrar Data" />
+                    </View>
+                    <View style={styleLista.viewCheckbox}>
+                        <Checkbox set={setShowExcluir} status={showExcluir} titulo="Mostrar Excluir" />
+                        <Checkbox set={setShowPreco} status={showPreco} titulo="Mostrar Preço" />
+                    </View>
+                </View>
                 <ScrollView
-                    horizontal={false}
+                    style={styleLista.scrollView}
+                    horizontal={true}
                 >
-                    <Titulo />
-                    {produtos.map((produto) => (
-                        <Item
-                            item={produto}
-                            setRefresh={setRefresh}
-                            key={produto.id}
-                        />
-                    ))}
+                    <ScrollView
+                        horizontal={false}
+                    >
+                        <Titulo checkbox={checkbox} />
+                        {produtos.map((produto) => (
+                            <Item
+                                item={produto}
+                                setRefresh={setRefresh}
+                                key={produto.id}
+                                checkbox={checkbox}
+                            />
+                        ))}
+                    </ScrollView>
                 </ScrollView>
-            </ScrollView>
+            </>
     );
 }
