8a53e664ecaf166537101b3df64ce7e24aa1e0b9
BrunoSerpa <serpabruno2005@gmail.com>
2024-11-01 10:33:39 -0300

    Criei os checkbox do histórico de cadastro

--- src/components/lists/listaCadastros/index.tsx
+++ src/components/lists/listaCadastros/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import { listarCadastros, listarVendas } from "../../../services";
 import { styleLista } from "../style";
 import Carregando from "../../carregando";
@@ -9,6 +9,8 @@ import { useFocusEffect } from "@react-navigation/native";
 import { VisualizarVenda } from "../../../interface/Venda";
 import ItemCadastro from "./itemCompra";
 import ItemVenda from "./itemVenda";
+import ICheckbox from "./interface";
+import { Checkbox } from "../../inputs";
 
 export default function ListaCadastros() {
     const [cadastros, setCadastros] = useState<VisualizarCadastro[] | undefined>(undefined);
@@ -39,11 +41,32 @@ export default function ListaCadastros() {
         }, [])
     );
 
+    const [showTitulo, setShowTitulo] = useState(true);
+    const [showData, setShowData] = useState(true);
+    const [showTotal, setShowTotal] = useState(true);
+    const [showFuncoes, setShowFuncoes] = useState(true);
+    const checkbox: ICheckbox = {
+        Título: showTitulo,
+        Data: showData,
+        Total: showTotal, 
+        Funcoes: showFuncoes,
+    }
     return (
         (cadastros === undefined || vendas === undefined) ?
             <Carregando />
             :
             <>
+                <View style={styleLista.viewCheckboxPrincipal}>
+                    <View style={styleLista.viewCheckbox}>
+                        <Checkbox set={setShowTitulo} status={showTitulo} titulo="Mostrar Título"/>
+                        <Checkbox set={setShowData} status={showData} titulo="Mostrar Data"/>
+                    </View>
+                    <View style={styleLista.viewCheckbox}>
+                        <Checkbox set={setShowTotal} status={showTotal} titulo="Mostrar Total"/>
+                        <Checkbox set={setShowFuncoes} status={showFuncoes} titulo="Mostrar Funções"/>
+                    </View>
+                
+                </View>
                 <ScrollView
                     style={styleLista.scrollView}
                     horizontal={true}
@@ -51,12 +74,13 @@ export default function ListaCadastros() {
                     <ScrollView
                         horizontal={false}
                     >
-                        <Titulo />
+                        <Titulo checkbox={checkbox} />
                         {cadastros.map((cadastro) => (
                             <ItemCadastro
                                 cadastro={cadastro}
                                 setRefresh={setRefresh}
                                 key={cadastro.id}
+                                checkbox={checkbox}
                             />
                         ))}
                         {vendas.map((venda) => (
@@ -64,6 +88,7 @@ export default function ListaCadastros() {
                                 venda={venda}
                                 setRefresh={setRefresh}
                                 key={venda.id}
+                                checkbox={checkbox}
                             />
                         ))}
                     </ScrollView>
--- src/components/lists/listaCadastros/titulo.tsx
+++ src/components/lists/listaCadastros/titulo.tsx
@@ -1,18 +1,35 @@
 import { View, Text } from "react-native";
 import { styleTitulo } from "../style";
+import ICheckbox from "./interface";
 
-export default function Titulo() {
+interface Props {
+    checkbox: ICheckbox;
+}
+
+export default function Titulo(titulo: Props) {
     return (
         <View style={styleTitulo.viewLinha}>
-            <Text style={[styleTitulo.textTitulo, styleTitulo.widthTitulo]}>Título</Text>
             <View style={styleTitulo.separator} />
-            <Text style={[styleTitulo.textTitulo, styleTitulo.widthData]}>Data</Text>
-            <View style={styleTitulo.separator} />
-            <Text style={[styleTitulo.textTitulo, styleTitulo.widthTotal]}>Total</Text>
-            <View style={styleTitulo.separator} />
-            <Text style={[styleTitulo.textTitulo, styleTitulo.widthFuncoes]}>Editar</Text>
-            <View style={styleTitulo.separator} />
-            <Text style={[styleTitulo.textTitulo, styleTitulo.widthFuncoes]}>Excluir</Text>
+            {titulo.checkbox.Título && <>
+                <Text style={[styleTitulo.textTitulo, styleTitulo.widthTitulo]}>Título</Text>
+                <View style={styleTitulo.separator} />
+            </>}
+            {titulo.checkbox.Data && <>
+                <Text style={[styleTitulo.textTitulo, styleTitulo.widthData]}>Data</Text>
+                <View style={styleTitulo.separator} />
+            </>}
+            {titulo.checkbox.Total && <>
+                <Text style={[styleTitulo.textTitulo, styleTitulo.widthTotal]}>Total</Text>
+                <View style={styleTitulo.separator} />
+            </>}
+            {titulo.checkbox.Funcoes && <>
+                <Text style={[styleTitulo.textTitulo, styleTitulo.widthFuncoes]}>Editar</Text>
+                <View style={styleTitulo.separator} />
+            </>}
+            {titulo.checkbox.Funcoes && <>
+                <Text style={[styleTitulo.textTitulo, styleTitulo.widthFuncoes]}>Excluir</Text>
+                <View style={styleTitulo.separator} />
+            </>}
         </View>
     );
 }
